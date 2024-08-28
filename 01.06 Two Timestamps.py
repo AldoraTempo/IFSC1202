be9f29511738f52e37ff2b52bd634310c9ec6796
AldoraTempo <tbmitchell2@ualr.edu>
2024-08-28 14:44:22 +0000

    updated

--- 01.06 Two Timestamps.py
+++ 01.06 Two Timestamps.py
@@ -1,7 +1,6 @@
-import time
-val1 = input("Enter time in HH:MM:SS\n")
-val2 = input("Enter time in HH:MM:SS\n")
-hour, min, sec = [int(i) for i in time.split(":")]
-difference = val2 - val1
+from datetime import time
+val1_str = input (time("", "", ""))
+val2_str = input (time("", "", ""))
+difference = val2_str - val1_str
 print(difference.total_seconds()) # time difference in total seconds
 print()
