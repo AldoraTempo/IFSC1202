1db8b878eea579c0ce7f7dac1bd5ac703759f69a
AldoraTempo <tbmitchell2@ualr.edu>
2024-08-28 20:36:34 +0000

    Updated

--- 01.07 School Desks.py
+++ 01.07 School Desks.py
@@ -1,4 +1,21 @@
-nos = input("Enter number of students")
-val1 = nos // 2
-rs = nos % val1
-print(val1)
+import math
+
+a = int (input("Enter number of students:"))
+print(a)
+b = int (input("Enter number of students:"))
+print(b)
+c = int (input("Enter number of students:"))
+print(c)
+
+
+deska = math.floor(a / 2)
+deskb = math.floor(b / 2)
+deskc = math.floor(c / 2)
+dtf = deska + deskb + deskc
+print(dtf)
+ea = a % 2
+eb = b % 2
+ec = c % 2
+edt = ea + eb + ec
+td = dtf + edt
+print(td)
