c7d820c51bef88587a99d0066e6b73b2397dae79
AldoraTempo <tbmitchell2@ualr.edu>
2024-09-04 23:06:34 +0000

    Updated

--- 02.Project.py
+++ 02.Project.py
@@ -7,9 +7,7 @@ print(b)
 c = float(input("Enter side 3: "))
 print(c)
 s = (a + b + c) / 2
-area = math.sqrt(s * (s * a) * (s * b) * (s * c))
-print(area)
 if a + b > c and a + c > b and b + c > a:
-        print("The area of the triangle is: area")
+        print(s)
 else:
         print("The lengths do not form a triangle")
