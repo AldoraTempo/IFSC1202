a6b03293a8676163c131cdc9fffd077cfc604a9c
AldoraTempo <tbmitchell2@ualr.edu>
2024-09-04 22:13:15 +0000

    Updated

--- 02.Project.py
+++ 02.Project.py
@@ -2,17 +2,17 @@ import math
 
 def triangle_area():
   
-a = float(input("Enter side 1 length: "))
-print(a)
-b = float(input("Enter side 2 length: "))
-print(b)
-c = float(input("Enter side 3 length: "))
-print(c)
+    a = float(input("Enter side 1 length: "))
+    print(a)
+    b = float(input("Enter side 2 length: "))
+    print(b)
+    c = float(input("Enter side 3 length: "))
+    print(c)
 
-s = (a + b + c) / 2
-print(s)
+    s = (a + b + c) / 2
+    print(s)
 
-area = math.sqrt(s * (s-a) * (s-b) * (s-c))
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
 
-print("Area of the triangle is: area
-triangle_area()
+    print("Area of the triangle is: area ")
+    triangle_area()
