72b27da6eaa4035b212bc9047ce57c0d4f1f690a
AldoraTempo <tbmitchell2@ualr.edu>
2024-09-11 11:00:07 -0500

    Create  03.Project.py

---
+++ 03.Project.py
@@ -0,0 +1,24 @@
+op = input("Enter an operator (+, -, *, /): ")
+print(op)
+o1 = float(input("Enter the first operand: "))
+print(o1)
+o2 = float(input("Enter the second operand: "))
+print(o2)
+
+if op == '+':
+  res = o1 + o2
+  print("{o1} + {o2} = {res}")
+elif op == '-':
+  res = o1 - o2
+  print("{o1} - {o2} = {res}")
+elif op == '*':
+  res = o1 * o2
+  print("{o1} * {o2} = {res}")
+elif op == '/':
+  if o2 != 0:
+    res = o1 / o2
+    print("{o1} / {o2} = {res}")
+  else:
+      print("Error")
+else:
+    print("Invalid Operator")
